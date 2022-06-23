661b2c21f2b7dc840cfbb960913950783af5401b
Donald Brooks Arum <brooksd079@gmail.com>
2022-06-24 01:11:07 +0300

    Global declaration of variable in the javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,23 @@
-//Global API variables
-// const config = {
-//     apikey: "edfd7aa4792af952873ca60f7f05e6a8",
-//     tmdbUrl: "https://api.themoviedb.org/3/",
-//     imageUrl: "https://image.tmdb.org/t/p/w1280"
-// }
+/*=========================================
+    Global Variable Declaration
+===========================================*/
 
-export const config = {
-    api_key: "edfd7aa4792af952873ca60f7f05e6a8",
-    api_base_url: 'https://api.themoviedb.org/3/',
-    image_base_url: 'https://image.tmdb.org/t/p/w1280'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=edfd7aa4792af952873ca60f7f05e6a8&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=edfd7aa4792af952873ca60f7f05e6a8&query="'
+
+const form = document.querySelector('.form')
+const search = document.querySelector('.search__bar')
+const main = document.querySelector('.main')
+const movieHeader = document.getElementById('movie__header')
+const notFound = document.querySelector('.not-found')
+//Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url){
+    
+    const response = await axios.get(url)
+    const data = await response.data
+    showMovies(data.results)
+    
 }
-const renderMoviesDiv = document.getElementById("movies")
-
-const BASE_URL = config.api_base_url
-const API_KEY = config.api_key
-
-export async function getPopularMovies(page = 1) {
-    let data = []
-    try {
-        const response = await fetch(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=${page}`)
-        const responseData = await response.json()
-        data = responseData?.results
-    } catch (error) {
-        
-    }
-    return data
-}
-
-export async function renderMovies() {
-    const movies = await getPopularMovies()
-    console.log(movies)
-    moviesDiv.innerHTML = movies?.map(movie => renderSingleMovie(movie)).join("")
-}
-
-function renderSingleMovie(movie) {
-    return (
-        `
-        <div class="col-4 col-lg-3 col-xl-2 p-1">
-            <img src="${config.image_base_url + movie?.poster_path}" class="img-fluid" >
-        </div>
-        `
-    )
-}
-
-function App() {
-    renderMovies()
-}
-App();  
